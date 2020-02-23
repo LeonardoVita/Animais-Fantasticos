918e2aaa0198286e91a701563d15586b600ecf3e
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2020-02-23 08:36:06 -0300

    faq list animation

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const tabMenu = document.querySelectorAll('.js-tab-menu li')
-const tabContent = document.querySelectorAll('.js-tab-content section')
+function initTabMenu() {
+  const tabMenu = document.querySelectorAll('.js-tab-menu li')
+  const tabContent = document.querySelectorAll('.js-tab-content section')
 
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index) {
-    tabContent.forEach((item) => {
-      item.classList.remove('ativo')
+  if (tabMenu.length && tabContent.length) {
+
+    function activeTab(index) {
+      tabContent.forEach((item) => {
+        item.classList.remove('ativo')
+      });
+      tabContent[index].classList.add('ativo')
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener('click', () => activeTab(index))
     });
-    tabContent[index].classList.add('ativo')
+
   }
 
-  tabMenu.forEach((item, index) => {
-    item.addEventListener('click', () => activeTab(index))
-  });
 }
+initTabMenu();
+
+function initFaqList() {
+  const faqDt = document.querySelectorAll('.js-faq-list dt')
+
+  if (faqDt.length) {
+
+    function showDD() {
+      this.classList.toggle('active')
+      this.nextElementSibling.classList.toggle('active')
+    }
+
+    faqDt.forEach((item) => {
+      item.addEventListener('click', showDD)
+    });
+
+  }
+
+}
+initFaqList();
